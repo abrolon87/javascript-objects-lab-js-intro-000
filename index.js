e343c7f93749c9f073863568c2bd9cb738555b76
Amanda R <abrolon87@gmail.com>
2019-08-14 21:37:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,40 +9,16 @@ function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
   recipes[key] = value
   return recipes
 }
-//passed
+
 function deleteFromObjectByKey(recipes, key) {
   var recipes = { key: 'value' }
   var newObj = Object.assign({}, recipes)
-
   delete newObj.key
   return recipes;
 }
-
+//passed
 function destructivelyDeleteFromObjectByKey() {
   delete recipes.key;
   return recipes;
 }
 
-/*var obj = { foo: 'bar' }
-
-var newObj = Object.assign({}, obj)
-
-newObj // { foo: 'bar' }
-
-delete newObj.foo // true
-
-newObj // {}
-
-obj // { foo: 'bar
-
-    expect(newObj['prop']).toBe(undefined)
-  })
-
-  it('does not modify the original object (it is non-destructive)', function() {
-    var obj = { prop: 1 }
-
-    deleteFromObjectByKey(obj, 'prop')
-    expect(obj['prop']).toBe(1)
-  })
-})
-*/
