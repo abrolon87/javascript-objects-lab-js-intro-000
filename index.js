3d5024d5978a38ea897b5293f2f29e72312583a2
Amanda R <abrolon87@gmail.com>
2019-08-14 21:41:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function deleteFromObjectByKey(recipes, key) {
 }
 //passed
 function destructivelyDeleteFromObjectByKey(recipes, key) {
-  var recipes = { key: 'value' }
+  var recipes = { key: 'value' };
   delete recipes.key;
   return recipes;
 }
