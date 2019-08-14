4cc8a4300b515a4d4b77745d232d85017b15b62e
Amanda R <abrolon87@gmail.com>
2019-08-14 21:32:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
 function deleteFromObjectByKey(recipes, key) {
   var key = { prop: 1}
   var newObj = Object.assign({}, key)
-  console.log newObj;
+  
   delete newObj.key
   return key
 
