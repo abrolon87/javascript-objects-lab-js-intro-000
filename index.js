05fc9efd140add13ce855228b967fafb4aa15a39
Amanda R <abrolon87@gmail.com>
2019-08-14 21:54:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var recipes = { key: 'value' }
+const recipes = { key: 'value' };
 
 function updateObjectWithKeyAndValue(recipes, key, value) {
   return Object.assign ({}, recipes, { [key]: value })
