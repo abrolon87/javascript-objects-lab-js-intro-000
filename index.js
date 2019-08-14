df1913cc62357d905119437e80723d45b9242f5c
Amanda R <abrolon87@gmail.com>
2019-08-14 20:40:23 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+const recipes = {};
+var key, value;
+function updateObjectWithKeyAndValue(recipes, key, value) {
+  return Object.assign ({}, recipes, { prop: 1, prop2: 2})
+
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+
+}
+
+function deleteFromObjectByKey(object, key) {
+
+}
+
+function destructivelyDeleteFromObjectByKey() {
+
+}
